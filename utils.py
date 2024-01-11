78bf0502f3d43c1871a4c2e20d7b7ca9d9bae7f8
Can Ozden <101993364+bysiber@users.noreply.github.com>
2024-01-11 05:43:29 +0300

    + new utils file

---
+++ utils.py
@@ -0,0 +1,22 @@
+
+import os
+def compatible_path(path):
+    """
+    -> This function takes a path as input and converts it to the appropriate format for the current OS.
+    -> This function returns path which is compatible with the OS. (Windows, Linux...)
+    -> \\ windows
+    -> / linux
+    """
+
+    if os.name == 'nt' and '/' in path:
+        # If the OS is Windows and the path uses Unix-style slashes, convert to Windows style
+        return os.path.normpath(path).replace("/", "\\")
+    elif os.name != 'nt' and '\\' in path:
+        # If the OS is not Windows and the path uses Windows-style backslashes, convert to Unix style
+        return os.path.normpath(path).replace("\\", "/")
+    else:
+        # If the path is already in the correct format for the OS, return it as is
+        return path
+
+
+print(compatible_path("Users\\user\\Desktop\\stegano\\resources\\test.txt"))
